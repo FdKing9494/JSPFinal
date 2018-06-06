cc1c9f1f32a40b9849120d4509808240e8ef7dfa
FdKing9494 <fengdofficial@gmail.com>
2018-06-06 15:52:27 +0800

    最后更新：主页优化、登录跳转

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # JSPFinal
 * 这是一份JSP结课小项目
-* 本项目完成了登录、注册、修改用户密码的基本功能
 * 项目前端：html
-  </br>项目后端：flask
+* 项目后端：Python框架flask
+* 数据存储：SQLite3
+
+
+=======================================
+* FCC证书链接：
+
+
+
+## 项目功能
+1. 用户注册
+2. 用户登录（登录失败错误提示、成功跳转）
+
+##部署方法
+### essential env
+(需要python 3及以上)
+<p><code>$pip install flask</code></p>
+<p><code>$pip install flask_sqlalchemy</code></p>
+
+### 访问
+http://127.0.0.1:5000
+
--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def home():
     if request.method == 'GET':
         return render_template('home.html')
 
-
 @app.route('/', methods = ['GET','POST'])
 def index():
     all_data = record.query.all()
@@ -55,61 +54,15 @@ def index():
         print(data)
         USERNAME = str(data['USERNAME'])
         PASSWORD = str(data['PASSWORD'])
-        x = record.query.filter_by(USERNAME = USERNAME).one()
-
-        if (str(x.PASSWORD) == PASSWORD):
-            print("welcome!")
+        try:
+            x = record.query.filter_by(USERNAME = USERNAME).one()
+            if (str(x.PASSWORD) == PASSWORD):
+                print("welcome!")
             return ''
-
-@app.route('/submit',methods = ['GET','POST','DELETE','PATCH'])
-def submit():
-    if request.method == 'GET':
-        return redirect('/')
-
-    if request.method == 'POST':
-        data = json.loads(request.data.decode('utf-8'))
-        try:
-            USERNAME = data['userid'].strip()
-            PASSWORD = data['psw']
-            if(USERNAME == '' or PASSWORD == ''):
-                raise ValueError
-            user = record(USERNAME = USERNAME,PASSWORD = PASSWORD)
-            db.session.add(user)
-        except ValueError:
-            return '用户名或密码未填写，请重新填写'
-        db.session.commit()
-        db.session.close()
-        print('写入成功！')
-        return ''
-
-    if request.method == 'DELETE':
-        data = json.loads(request.data.decode('utf-8'))
-        USERNAME = data['userid'].strip()
-        print(USERNAME)
-        all_data = record.query.all()
-        try:
-            x = record.query.filter_by(USERNAME = USERNAME).one()
-            db.session.delete(x)
         except:
             pass
-        db.session.commit()
-        return ''
+            return 'ERROR'
 
-    if request.method == 'PATCH':
-        data = json.loads(request.data.decode('utf-8'))
-        USERNAME = data['userid'].strip()
-        PASSWORD = data['psw']
-        if(USERNAME == '' or PASSWORD == ''):
-            raise ValueError
-        try:
-            x = record.query.filter_by(USERNAME = USERNAME).one()
-            x.PASSWORD = PASSWORD
-        except:
-            pass
-        finally:
-            db.session.close()
-        print('提交成功！')
-        return ''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
